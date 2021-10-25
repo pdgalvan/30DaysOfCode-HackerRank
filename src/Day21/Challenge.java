c21a048356402040996f55c4bca17dcb06e20caf
Pablo Daniel Galvan Vergara <gvpablodaniel@gmail.com>
2021-10-25 00:58:21 -0300

    day21 completed

--- src/Day21/Challenge.java
+++ src/Day21/Challenge.java
@@ -1,4 +1,13 @@
 package Day21;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Challenge {
+    public static <T> void printArray(T[] list){
+        for(T item : list){
+            System.out.println(item);
+        }
+    }
 }
